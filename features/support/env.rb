352b33e0c4a3ced22d0d2f5f91733bb995abb9b5
Joanna <joannamrt96@gmail.com>
2023-06-29 21:44:53 +0700

    Modify environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,7 @@ require 'capybara/cucumber'
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument("--start-maximized")
+options.add_argument("log-level=3")
 
 Capybara.register_driver :selenium_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
